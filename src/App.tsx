eaf1a2c1ae53255859b49973ed5463a44ccdb3d3
alexf <alexf@tut.by>
2025-02-18 11:36:15 +0300

    map added

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,23 @@ export type TasksType = {
   isDone: boolean;
 };
 
-const Tasks1 = [
+const Tasks1: TasksType[]  = [
   { id: 1, title: "HTML&CSS", isDone: true },
   { id: 2, title: "JS", isDone: true },
   { id: 3, title: "React", isDone: false },
 ];
-const Tasks2 = [
+const Tasks2: TasksType[] = [
   { id: 1, title: "Oh, my Niga!", isDone: true },
   { id: 2, title: "Suck My Disk", isDone: true },
   { id: 3, title: "Lick my ass", isDone: false },
+  { id: 4, title: "I now, you are cock sucker", isDone: true },
+
 ];
 
 export const App = () => {
   return (
     <div className="app">
-      <TodoListItem title="What to learn" tasks={Tasks1} />
+      <TodoListItem title="What to learn" tasks={Tasks1} date="01.01.2023"/>
       <TodoListItem title="Songs" tasks={Tasks2} />
       {/* <TodoListItem title="Books"/> */}
     </div>
--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,34 +1,35 @@
 import { TasksType } from "./App";
 
 type TodoListItemPropsType = {
-    title: string;
-    tasks: TasksType[] 
-}
+  title: string;
+  tasks: TasksType[];
+  date?: string;
+};
 
-export const TodoListItem = ({title, tasks}: TodoListItemPropsType) => {
-    return (
-        <div>
-        <h3>{title}</h3>
-        <div>
-          <input/>
-          <button>+</button>
-        </div>
-        <ul>
-          <li>
-            <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-          </li>
-          <li>
-            <input type="checkbox" checked={true}/> <span>JS</span>
-          </li>
-          <li>
-            <input type="checkbox" checked={false}/> <span>React</span>
-          </li>
-        </ul>
-        <div>
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
-        </div>
+export const TodoListItem = ({ title, tasks, date }: TodoListItemPropsType) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div>
+        <input />
+        <button>+</button>
       </div>
-    )
-}
+      <ul>
+        {tasks.map((t) => {
+          return (
+            <li>
+              <input type="checkbox" checked={t.isDone} />{" "}
+              <span>{t.title}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+      <div>{date ? `Creation date: ${date}` : ""}</div>
+    </div>
+  );
+};
